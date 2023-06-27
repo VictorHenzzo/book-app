8f8a720357866426f84a3aa09ae2e3cdd0ac4770
Victor H <victorhenzzowp@gmail.com>
2023-06-27 11:06:02 -0300

    Adding service locator

---
+++ lib/core/infra/service_locator/service_locator.dart
@@ -0,0 +1,20 @@
+import 'package:book_app/core/infra/service_locator/service_locator.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+@InjectableInit()
+void _configureDependencies(GetIt getIt) => getIt.init();
+
+class ServiceLocator {
+  ServiceLocator._();
+
+  static final GetIt _getIt = GetIt.instance;
+
+  static T provide<T extends Object>() {
+    return _getIt.get<T>();
+  }
+
+  static void init() {
+    _configureDependencies(_getIt);
+  }
+}
