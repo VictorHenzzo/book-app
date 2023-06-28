e8d2a4883d190136c8862321bad01b0139824ed7
Victor H <victorhenzzowp@gmail.com>
2023-06-28 16:46:15 -0300

    Extracting error widget from home screen module

--- lib/modules/home/ui/widgets/error_state_widget/home_screen_error_state_widget.dart
+++ lib/modules/home/ui/widgets/error_state_widget/home_screen_error_state_widget.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/widgets/custom_bottom_navigation_bar/custom_bottom_navigation_bar.dart';
+import 'package:book_app/widgets/error/default_error_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreenErrorStateWidget extends StatelessWidget {
@@ -10,74 +11,16 @@ class HomeScreenErrorStateWidget extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       bottomNavigationBar: const CustomBottomNavigationBar(),
       body: Center(
-        child: _ErrorContainer(
+        child: DefaultErrorWidget(
           tryAgain: tryAgain,
+          width: screenSize.width * 0.8,
+          heigth: screenSize.height * 0.5,
         ),
-      ),
-    );
-  }
-}
-
-class _ErrorContainer extends StatelessWidget {
-  const _ErrorContainer({
-    required this.tryAgain,
-  });
-
-  final VoidCallback tryAgain;
-
-  @override
-  Widget build(final BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final textTheme = Theme.of(context).textTheme;
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return Container(
-      height: screenSize.height * 0.5,
-      width: screenSize.width * 0.8,
-      decoration: BoxDecoration(
-        color: colorScheme.errorContainer,
-        borderRadius: BorderRadius.circular(60),
-      ),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Spacer(flex: 5),
-          Icon(
-            Icons.error,
-            size: screenSize.height * 0.1,
-            color: colorScheme.error,
-          ),
-          const Spacer(),
-          Text(
-            'Oops, algo deu errado!',
-            overflow: TextOverflow.ellipsis,
-            style: textTheme.titleMedium?.copyWith(
-              color: colorScheme.error,
-            ),
-          ),
-          const Spacer(),
-          Text(
-            'Desculpe, não foi possível carregar o conteúdo neste momento.\nPor favor, verifique sua conexão com a internet e tente novamente.',
-            style: textTheme.bodyMedium,
-            textAlign: TextAlign.center,
-          ),
-          const Spacer(),
-          ElevatedButton(
-            onPressed: tryAgain,
-            child: Text(
-              'Tentar Novamente',
-              overflow: TextOverflow.ellipsis,
-              style: textTheme.labelLarge,
-            ),
-          ),
-          const Spacer(flex: 5),
-        ],
       ),
     );
   }
---
+++ lib/widgets/error/default_error_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class DefaultErrorWidget extends StatelessWidget {
+  const DefaultErrorWidget({
+    required this.tryAgain,
+    required this.width,
+    required this.heigth,
+  }) : super(key: const Key('defaultErrorWidget'));
+
+  final VoidCallback tryAgain;
+  final double width;
+  final double heigth;
+
+  @override
+  Widget build(final BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      height: heigth,
+      width: width,
+      decoration: BoxDecoration(
+        color: colorScheme.errorContainer,
+        borderRadius: BorderRadius.circular(60),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(flex: 5),
+          Icon(
+            Icons.error,
+            size: heigth * 0.1,
+            color: colorScheme.error,
+          ),
+          const Spacer(),
+          Text(
+            'Oops, algo deu errado!',
+            overflow: TextOverflow.ellipsis,
+            style: textTheme.titleMedium?.copyWith(
+              color: colorScheme.error,
+            ),
+          ),
+          const Spacer(),
+          Text(
+            'Desculpe, não foi possível carregar o conteúdo neste momento.\nPor favor, verifique sua conexão com a internet e tente novamente.',
+            style: textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(),
+          ElevatedButton(
+            onPressed: tryAgain,
+            child: Text(
+              'Tentar Novamente',
+              overflow: TextOverflow.ellipsis,
+              style: textTheme.labelLarge,
+            ),
+          ),
+          const Spacer(flex: 5),
+        ],
+      ),
+    );
+  }
+}
