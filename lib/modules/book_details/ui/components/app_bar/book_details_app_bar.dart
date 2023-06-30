8835213a01f26d0ebceb5cd64831417b967fe737
Victor H <victorhenzzowp@gmail.com>
2023-06-30 12:06:46 -0300

    Improving theme usave

--- lib/modules/book_details/ui/components/app_bar/book_details_app_bar.dart
+++ lib/modules/book_details/ui/components/app_bar/book_details_app_bar.dart
@@ -9,17 +9,19 @@ class BookDetailsAppBar extends StatelessWidget implements PreferredSizeWidget {
   //TODO Usar sliver pra dar uma animação interessante, colocar a foto aqui
   @override
   PreferredSizeWidget build(final BuildContext context) {
+    final iconColor = Theme.of(context).colorScheme.surface;
+
     return AppBar(
       backgroundColor: Colors.transparent,
       iconTheme: IconThemeData(
-        color: Theme.of(context).colorScheme.surface,
+        color: iconColor,
       ),
       actions: [
         IconButton(
           onPressed: () {},
           icon: Icon(
             Icons.more_vert,
-            color: Theme.of(context).colorScheme.surface,
+            color: iconColor,
           ),
         ),
       ],
